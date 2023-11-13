ea8e400bb5d5506c13f6998cbf8c3907044e0a93
rars1714 <renoraso@outlook.com>
2023-11-12 23:38:11 -0600

    Se modifico el título de la página

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>new era cap</title>
+    <title>NEW ERA CAP SHOP</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
